c2718a5c92de39d9cdcdca9ff8a77861e1c8e248
Denis Snitko <denissnitko@gmail.com>
2021-06-24 17:35:42 +0300

    Minor refactoring

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { Context } from './context'
 export default function App() {
   const [text, setText] = React.useState('Начальное состояние')
 
+  const changeState = () => {
+    setText(randomInteger(1000000, 99999999999))
+  }
+
   const randomInteger = (min, max) => {
     let rand = min + Math.random() * (max + 1 - min)
     return Math.floor(rand)
   }
 
-  const changeState = () => {
-    setText(randomInteger(1000000, 99999999999))
-  }
   return (
     <Context.Provider value={{text, changeState}}>
       <div className='App'>
